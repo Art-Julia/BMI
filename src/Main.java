938d1d72d5db5544a0fad33086304ccaa2eb6aa2
Yulia Arutyunyan <artn.julia@gmail.com>
2021-05-31 22:31:16 +0300

    changed function for rounding

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(50,1.73);
-        System.out.println(bmi);
+        double bmi = (int)service.calculate(50,1.73);
+        System.out.printf("%.2f", bmi);
     }
 
 }
